634a605a4342cfcd7aa4ddf6818a230b74d639b1
hikikomoriphoenix <dondon1987@yandex.com>
2018-03-14 15:58:24 +0800

    Initial commit

---
+++ app/src/main/java/marabillas/loremar/lmvideodownloader/LMvd.java
@@ -0,0 +1,13 @@
+package marabillas.loremar.lmvideodownloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LMvd extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
