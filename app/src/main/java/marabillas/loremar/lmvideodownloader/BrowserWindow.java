80012159cbbff1a09cb7722af3724d5f8632646b
hikikomoriphoenix <dondon1987@yandex.com>
2018-03-16 14:16:06 +0800

    detect videos

--- app/src/main/java/marabillas/loremar/lmvideodownloader/BrowserWindow.java
+++ app/src/main/java/marabillas/loremar/lmvideodownloader/BrowserWindow.java
@@ -26,6 +26,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,16 +39,33 @@ import android.widget.TextView;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSocketFactory;
+
 public class BrowserWindow extends Fragment {
-    String url;
-    View view;
-    WebView page;
+    private String url;
+    private String title;
+    private View view;
+    private WebView page;
+    private List<Video> videos;
+
+    private static final String TAG = "loremarTest";
+
+    class Video{
+        String size, type, link, name;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle data = getArguments();
         url = data.getString("url");
+        videos = new ArrayList<>();
     }
 
     @Override
@@ -86,15 +104,63 @@ public class BrowserWindow extends Fragment {
             }
 
             @Override
-            public void onPageStarted(WebView view, final String url, Bitmap favicon) {
+            public void onPageStarted(final WebView view, final String url, Bitmap favicon) {
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
                         TextView urlBox = BrowserWindow.this.view.findViewById(R.id.urlBox);
                         urlBox.setText(url);
+                        BrowserWindow.this.title = view.getTitle();
                     }
                 });
                 super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onLoadResource(WebView view, final String url) {
+                new Thread(){
+                    @Override
+                    public void run() {
+                        String urlLowerCase = url.toLowerCase();
+                        if(urlLowerCase.contains("mp4")||urlLowerCase.contains("video")){
+                            HttpsURLConnection uCon = null;
+                            try {
+                                uCon = (HttpsURLConnection) new URL(url).openConnection();
+                                uCon.setSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
+                                uCon.connect();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            if (uCon != null) {
+                                String contentType = uCon.getHeaderField("content-type");
+                                if(contentType!=null) {
+                                    contentType = contentType.toLowerCase();
+                                    if (contentType.contains("video/mp4")) {
+                                        Video video = new Video();
+                                        video.size = uCon.getHeaderField("content-length");
+                                        String link = uCon.getHeaderField("Location");
+                                        if (link == null) {
+                                            link = uCon.getURL().toString();
+                                        }
+                                        video.link = link;
+                                        if (title != null) {
+                                            video.name = title;
+                                        } else {
+                                            video.name = "video";
+                                        }
+                                        video.type = "mp4";
+                                        videos.add(video);
+                                        String videoFound = "name:" + video.name + "\n" +
+                                                "link:" + video.link + "\n" +
+                                                "type:" + video.type + "\n" +
+                                                "size" + video.size;
+                                        Log.i(TAG, videoFound);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }.start();
             }
         });
         page.loadUrl(url);
