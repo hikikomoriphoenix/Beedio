40ded49bb4c543c8189cfbbf477f160a6e14ef00
hikikomoriphoenix <dondon1987@yandex.com>
2018-04-15 16:41:41 +0800

    DownloadsCompleted update

--- app/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/DownloadsCompleted.java
+++ app/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/DownloadsCompleted.java
@@ -20,8 +20,65 @@
 
 package marabillas.loremar.lmvideodownloader.download_feature;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import marabillas.loremar.lmvideodownloader.LMvdFragment;
+import marabillas.loremar.lmvideodownloader.R;
 
 public class DownloadsCompleted extends LMvdFragment {
     //todo implement DownloadsCompleted class
+    private RecyclerView downloadsList;
+    private TextView clearAllFinishedButton;
+    private List<String> videos;
+    private CompletedVideos completedVideos;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.downloads_completed, container,false);
+
+        downloadsList = view.findViewById(R.id.downloadsCompletedList);
+        clearAllFinishedButton = view.findViewById(R.id.clearAllFinishedButton);
+
+        videos = new ArrayList<>();
+        File file = new File(getActivity().getFilesDir(), "completed.dat");
+        if (file.exists()) {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                completedVideos = (CompletedVideos) objectInputStream.readObject();
+                videos = completedVideos.getVideos();
+                objectInputStream.close();
+                fileInputStream.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //todo downloadsList settings
+
+        //todo clearAllFinishedButton handle click input
+
+        return view;
+    }
+
+    //todo DownloadsCompleted adapter and viewholder
 }
