d99f2e559d4982637d7950fe5c2374dd65ebb122
hikikomoriphoenix <dondon1987@yandex.com>
2018-05-04 17:30:58 +0800

    download notice

--- app/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/Downloads.java
+++ app/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/Downloads.java
@@ -44,7 +44,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
-import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
