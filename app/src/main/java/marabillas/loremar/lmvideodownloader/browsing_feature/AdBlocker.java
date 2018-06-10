eb3b5f84474aa391612e4aba6f4d36105afa6581
hikikomoriphoenix <dondon1987@yandex.com>
2018-06-10 15:19:39 +0800

    minimize updating ad filters

--- app/src/main/java/marabillas/loremar/lmvideodownloader/browsing_feature/AdBlocker.java
+++ app/src/main/java/marabillas/loremar/lmvideodownloader/browsing_feature/AdBlocker.java
@@ -22,6 +22,7 @@ package marabillas.loremar.lmvideodownloader.browsing_feature;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -34,8 +35,13 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import marabillas.loremar.lmvideodownloader.R;
 
 class AdBlocker implements Serializable {
     private List<String> filters;
@@ -46,56 +52,61 @@ class AdBlocker implements Serializable {
     }
 
     public void update(final Context context) {
-        final AlertDialog dialog = new AlertDialog.Builder(context).create();
-        dialog.setMessage("Updating. Please wait...");
-        dialog.setCancelable(false);
-        dialog.setCanceledOnTouchOutside(false);
-        dialog.show();
-        new Thread() {
-            @Override
-            public void run() {
-                String easyList = "https://easylist.to/easylist/easylist.txt";
-                List<String> tempFilters = new ArrayList<>();
-                try {
-                    URLConnection uCon = new URL(easyList).openConnection();
-                    if (uCon != null) {
-                        InputStream in = uCon.getInputStream();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            if (line.contains("Last modified")) {
-                                if (line.equals(easylistLastModified)) {
-                                    Log.i("loremarTest", "ads filters is already up to date");
-                                    reader.close();
-                                    in.close();
-                                    dialog.dismiss();
-                                    return;
-                                } else {
-                                    easylistLastModified = line;
+        SharedPreferences prefs = context.getSharedPreferences("settings", 0);
+        String today = new SimpleDateFormat("dd MM yyyy", Locale.getDefault()).format(new Date());
+        if (!today.equals(prefs.getString(context.getString(R.string.adFiltersLastUpdated), ""))) {
+            final AlertDialog dialog = new AlertDialog.Builder(context).create();
+            dialog.setMessage("Updating. Please wait...");
+            dialog.setCancelable(false);
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+            new Thread() {
+                @Override
+                public void run() {
+                    String easyList = "https://easylist.to/easylist/easylist.txt";
+                    List<String> tempFilters = new ArrayList<>();
+                    try {
+                        URLConnection uCon = new URL(easyList).openConnection();
+                        if (uCon != null) {
+                            InputStream in = uCon.getInputStream();
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                if (line.contains("Last modified")) {
+                                    if (line.equals(easylistLastModified)) {
+                                        Log.i("loremarTest", "ads filters is already up to date");
+                                        reader.close();
+                                        in.close();
+                                        dialog.dismiss();
+                                        return;
+                                    } else {
+                                        easylistLastModified = line;
+                                    }
+                                } else if (!line.startsWith("!") || !line.startsWith("[")) {
+                                    tempFilters.add(line);
                                 }
-                            } else if (!line.startsWith("!") || !line.startsWith("[")) {
-                                tempFilters.add(line);
                             }
+                            if (!tempFilters.isEmpty()) {
+                                filters = tempFilters;
+                                Log.i("loremarTest", "updating ads filters complete. Total: " +
+                                        filters.size());
+                            }
+                            File file = new File(context.getFilesDir(), "ad_filters.dat");
+                            FileOutputStream fileOutputStream = new FileOutputStream(file);
+                            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                            objectOutputStream.writeObject(AdBlocker.this);
+                            objectOutputStream.close();
+                            fileOutputStream.close();
                         }
-                        if (!tempFilters.isEmpty()) {
-                            filters = tempFilters;
-                            Log.i("loremarTest", "updating ads filters complete. Total: " +
-                                    filters.size());
-                        }
-                        File file = new File(context.getFilesDir(), "ad_filters.dat");
-                        FileOutputStream fileOutputStream = new FileOutputStream(file);
-                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-                        objectOutputStream.writeObject(AdBlocker.this);
-                        objectOutputStream.close();
-                        fileOutputStream.close();
-                    }
-                    dialog.dismiss();
+                        dialog.dismiss();
 
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        }.start();
+            }.start();
+        }
+        prefs.edit().putString(context.getString(R.string.adFiltersLastUpdated), today).apply();
     }
 
     public boolean checkThroughFilters(String url) {
