7e0eaff452524fe35dba8a4694db623e8a2ac320
Loremar Marabillas <loremar5858@gmail.com>
2019-09-30 04:58:16 +0800

    Add missing copyright notices

--- legacy/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/fragments/Downloads.java
+++ legacy/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/fragments/Downloads.java
@@ -1,4 +1,23 @@
 /*
+ * Beedio is an Android app for downloading videos
+ * Copyright (C) 2019 Loremar Marabillas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
  *     LM videodownloader is a browser app for android, made to easily
  *     download videos.
  *     Copyright (C) 2018 Loremar Marabillas
@@ -310,7 +329,7 @@ public class Downloads extends LMvdFragment implements LMvdActivity.OnBackPresse
                         tab.setText(tabText);
                     }
                 } else {*/
-                    inProgressTab.setText(tabText);
+                inProgressTab.setText(tabText);
             }
         });
     }
@@ -329,7 +348,7 @@ public class Downloads extends LMvdFragment implements LMvdActivity.OnBackPresse
                         tab.setText(tabText);
                     }
                 } else {*/
-                    completedTab.setText(tabText);
+                completedTab.setText(tabText);
             }
         });
     }
@@ -348,7 +367,7 @@ public class Downloads extends LMvdFragment implements LMvdActivity.OnBackPresse
                         tab.setText(tabText);
                     }
                 } else {*/
-                    inactiveTab.setText(tabText);
+                inactiveTab.setText(tabText);
             }
         });
     }
--- legacy/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/fragments/DownloadsInactive.java
+++ legacy/src/main/java/marabillas/loremar/lmvideodownloader/download_feature/fragments/DownloadsInactive.java
@@ -1,4 +1,23 @@
 /*
+ * Beedio is an Android app for downloading videos
+ * Copyright (C) 2019 Loremar Marabillas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*
  *     LM videodownloader is a browser app for android, made to easily
  *     download videos.
  *     Copyright (C) 2018 Loremar Marabillas
@@ -43,7 +62,6 @@ package marabillas.loremar.lmvideodownloader.download_feature.fragments;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Environment;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.util.TypedValue;
