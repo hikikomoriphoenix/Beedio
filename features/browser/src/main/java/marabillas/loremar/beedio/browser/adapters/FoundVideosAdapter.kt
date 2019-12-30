5d3a503e6eb95bbf885655a9812554ddb7322040
Loremar Marabillas <loremar5858@gmail.com>
2019-12-30 17:25:59 +0800

    Add more details to expand and collapse

--- features/browser/src/main/java/marabillas/loremar/beedio/browser/adapters/FoundVideosAdapter.kt
+++ features/browser/src/main/java/marabillas/loremar/beedio/browser/adapters/FoundVideosAdapter.kt
@@ -20,19 +20,22 @@
 package marabillas.loremar.beedio.browser.adapters
 
 import android.animation.ValueAnimator
+import android.graphics.Typeface
 import android.text.format.Formatter
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams.PARENT_ID
 import androidx.core.animation.doOnEnd
 import androidx.core.animation.doOnStart
-import androidx.core.view.doOnLayout
-import androidx.core.view.doOnNextLayout
-import androidx.core.view.doOnPreDraw
-import androidx.core.view.updateLayoutParams
+import androidx.core.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
 import marabillas.loremar.beedio.browser.R
 import marabillas.loremar.beedio.browser.databinding.FoundVideosListItemBinding
 import marabillas.loremar.beedio.browser.viewmodel.VideoDetectionVM
@@ -123,17 +126,41 @@ class FoundVideosAdapter : RecyclerView.Adapter<FoundVideosAdapter.FoundVideosVi
         }
 
         private fun expandItem() {
+            binding.apply {
+                foundVideoSize.updateLayoutParams<ConstraintLayout.LayoutParams> {
+                    topToBottom = -1
+                    bottomToBottom = PARENT_ID
+                    bottomMargin = (8 * itemView.resources.displayMetrics.density).roundToInt()
+                }
+                foundVideoName.apply {
+                    setTypeface(typeface, Typeface.BOLD)
+                }
+            }
+
             ValueAnimator.ofFloat(0f, 1f).apply {
                 addUpdateListener {
                     val value = it.animatedValue as Float
-                    itemView.updateLayoutParams<ViewGroup.LayoutParams> {
-                        height = (origHeight + value * (expandedHeight - origHeight)).roundToInt()
+                    itemView.apply {
+                        updateLayoutParams<ViewGroup.LayoutParams> {
+                            height = (origHeight + value * (expandedHeight - origHeight)).roundToInt()
+                        }
                     }
+                    transformPadding(value)
+                    transformIcon(value)
                 }
                 doOnStart { setContentsVisibility(true) }
                 doOnEnd {
                     setContentsVisibility(true)
                     isExpanded = true
+                    binding.foundVideoSize.apply {
+                        visibility = GONE
+                        updateLayoutParams<ConstraintLayout.LayoutParams> {
+                            startToEnd = -1
+                            startToStart = PARENT_ID
+                            marginStart = 0
+                        }
+                    }
+                    showSize()
                 }
             }.start()
         }
@@ -142,15 +169,58 @@ class FoundVideosAdapter : RecyclerView.Adapter<FoundVideosAdapter.FoundVideosVi
             ValueAnimator.ofFloat(1f, 0f).apply {
                 addUpdateListener {
                     val value = it.animatedValue as Float
-                    itemView.updateLayoutParams<ViewGroup.LayoutParams> {
-                        height = (origHeight + value * (expandedHeight - origHeight)).roundToInt()
+                    itemView.apply {
+                        updateLayoutParams<ViewGroup.LayoutParams> {
+                            height = (origHeight + value * (expandedHeight - origHeight)).roundToInt()
+                        }
                     }
+                    transformPadding(value)
+                    transformIcon(value)
                 }
                 doOnEnd {
                     setContentsVisibility(false)
                     isExpanded = false
+                    binding.apply {
+                        foundVideoSize.apply {
+                            visibility = GONE
+                            updateLayoutParams<ConstraintLayout.LayoutParams> {
+                                startToStart = -1
+                                startToEnd = R.id.found_video_icon
+                                marginStart = (16 * itemView.resources.displayMetrics.density).roundToInt()
+                                bottomToBottom = -1
+                                topToBottom = R.id.found_video_name
+                                bottomMargin = 0
+                            }
+                        }
+                        foundVideoName.apply {
+                            typeface = Typeface.create(typeface, Typeface.NORMAL)
+                        }
+                        showSize()
+                    }
                 }
             }.start()
+        }
+
+        private fun transformPadding(value: Float) {
+            itemView.apply {
+                val px8 = (8 * resources.displayMetrics.density).roundToInt()
+                val px16 = (16 * resources.displayMetrics.density).roundToInt()
+                val newPadding = (px16 - (px16 - px8) * value).roundToInt()
+                setPadding(newPadding, px16, newPadding, px16)
+            }
+        }
+
+        private fun transformIcon(value: Float) {
+            binding.foundVideoIcon.apply {
+                val px32 = (32 * resources.displayMetrics.density).roundToInt()
+                val px40 = (40 * resources.displayMetrics.density).roundToInt()
+                val newIconSize = (px32 + (px40 - px32) * value).roundToInt()
+                updateLayoutParams<ViewGroup.LayoutParams> {
+                    width = newIconSize
+                    height = newIconSize
+                }
+                setPadding((4 + 4 * value).roundToInt())
+            }
         }
 
         private fun setContentsVisibility(isVisible: Boolean) {
@@ -161,6 +231,11 @@ class FoundVideosAdapter : RecyclerView.Adapter<FoundVideosAdapter.FoundVideosVi
                 foundVideoRename.visibility = visibility
                 foundVideoDownload.visibility = visibility
             }
+        }
+
+        private fun showSize() {
+            TransitionManager.beginDelayedTransition(itemView as ViewGroup, Slide(Gravity.BOTTOM))
+            binding.foundVideoSize.visibility = VISIBLE
         }
     }
 
